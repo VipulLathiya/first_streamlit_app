7470c45ede7fcf5584a5f2da62ef27d354cfd230
VipulLathiya <135737890+VipulLathiya@users.noreply.github.com>
2023-06-06 19:06:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
-
-streamlit.title('My son has very good day today!!')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title(' 🥣 My son has very good day today!!')
+streamlit.header(' 🥗 Breakfast Menu')
+streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞 Hard-Boiled Free-Range Egg')
