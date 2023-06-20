3b6fc1477dfe11f31f6e4ee59a4d700dbf8405eb
VipulLathiya <135737890+VipulLathiya@users.noreply.github.com>
2023-06-20 18:21:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("Hello from Snowflake:")
 streamlit.dataframe(my_data_row)
-
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
